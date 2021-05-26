ec8a8d9eeecab0b8b02e2bb3fe1bc40196608631
pwzy <2386900869@qq.com>
2021-05-27 01:00:40 +0800

    cd

--- lisp/init-eaf.el
+++ lisp/init-eaf.el
@@ -17,7 +17,11 @@
   (eaf-bind-key scroll_up "C-n" eaf-pdf-viewer-keybinding)
   (eaf-bind-key scroll_down "C-p" eaf-pdf-viewer-keybinding)
   (eaf-bind-key take_photo "p" eaf-camera-keybinding)
-  (eaf-bind-key nil "M-q" eaf-browser-keybinding)) ;; unbind, see more in the Wiki
+  (eaf-bind-key nil "M-q" eaf-browser-keybinding) ;; unbind, see more in the Wiki
 
+  (setq eaf-proxy-type "http")
+  (setq eaf-proxy-host "127.0.0.1")
+  (setq eaf-proxy-port "7890")
+  )
 
 (provide 'init-eaf)
